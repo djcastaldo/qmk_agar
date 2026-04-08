c9c9ec0fdff1c62e3909158beb3f818d9dc0c731
Dave Castaldo <djcastaldo@gmail.com>
2026-04-08 15:49:03 -0400

    add RCMD_TD and RALT_TD, alteratives to TD(RCMD_OSL) and TD(RALT_OSL) to be used on older qmk like the Agar

--- keyboards/ydkb/agar/keymaps/djcastaldo/keymap.c
+++ keyboards/ydkb/agar/keymaps/djcastaldo/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TMUX_LT,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, BSPCFAST,
         FN_HHKB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_TD, OSL_FKEY,
-        _______, KC_LOPT, KC_LCMD, KC_SPC,  TD(RCMD_OSL), SP_RCTL, _______
+        _______, KC_LOPT, KC_LCMD, KC_SPC,  RCMD_TD, SP_RCTL, _______
     ),
 
     [WIN_BASE] = LAYOUT_60_tsangan_hhkb(
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TMUX_LT, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, BSPCFAST,
         FN_HHKB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_TD, OSL_FKEY,
-        _______, TD(LGUI_OSL), KC_LALT, KC_SPC,  TD(RALT_OSL), SP_RCTL, _______
+        _______, TD(LGUI_OSL), KC_LALT, KC_SPC,  RALT_TD, SP_RCTL, _______
     ),
 
     [FKEY_LAYR] = LAYOUT_60_tsangan_hhkb(
