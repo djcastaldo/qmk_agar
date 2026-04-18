072d5f3d05faa0db30f164917f0b2694604c7424
Dave Castaldo <djcastaldo@gmail.com>
2026-04-17 23:49:38 -0400

    moved pgup next to pgdn in keymap nav layers

--- keyboards/ydkb/agar/keymaps/djcastaldo/keymap.c
+++ keyboards/ydkb/agar/keymaps/djcastaldo/keymap.c
@@ -39,16 +39,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_60_tsangan_hhkb(
         DUAL_ESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, DYN_LT,
         TMUX_LT, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, BSPCFAST,
-        FN_HHKB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        FN_HHKB_2, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_TD, OSL_FKEY,
         _______, TD(LGUI_OSL), KC_LALT, KC_SPC,  RALT_TD, SP_RCTL, _______
     ),
 
     [FKEY_LAYR] = LAYOUT_60_tsangan_hhkb(
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_F13, KC_F14,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP,   KC_HOME, KC_END, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   KC_HOME, KC_END, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______,   KC_LEFT, KC_DOWN, KC_RGHT, _______,
-        _______, _______, _______,   _______, _______, _______, _______, _______,  _______, KC_PGDN, DUAL_SNAP, _______, _______,
+        _______, _______, _______,   _______, _______, _______, _______, _______,  KC_PGUP, KC_PGDN, DUAL_SNAP, _______, _______,
         _______, _______, _______, _______, AP_GLOB, _______, _______
     ),
 
@@ -92,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [TMUX_LAYR_NAV] = LAYOUT_60_tsangan_hhkb(
         TMUXLKEY, LTRANS, LTRANS, LTRANS, LTRANS, LTRANS, LTRANS, LTRANS, LTRANS, LTRANS, LTRANS, LTRANS, LTRANS, TWINLFT, TWINRGT,
-        _______, LTRANS, LTRANS, _______, _______, LTRANS, _______, _______, _______, TKC_PGUP, TKC_UP, LTRANS, LTRANS, TMUXLCMD,
+        _______, LTRANS, LTRANS, _______, _______, LTRANS, _______, _______, _______, LTRANS, TKC_UP, LTRANS, LTRANS, TMUXLCMD,
         _______, _______, LTRANS, LTRANS, _______, _______, _______, TJPANE, _______, TKC_LEFT, TKC_DOWN, TKC_RGHT, _______,
-        _______, LTRANS, LTRANS, LTRANS, _______, _______, LTRANS, ENC_TMON, LTRANS, KC_PGDN, LTRANS, _______, _______,
+        _______, LTRANS, LTRANS, LTRANS, _______, _______, LTRANS, ENC_TMON, TKC_PGUP, KC_PGDN, LTRANS, _______, _______,
         _______, KC_LALT, KC_LALT, LTRANS, KC_LALT, _______, _______
     ),
 
