e08482a53c27c6fb61dbd3e75927430534218f8e
Dave Castaldo <djcastaldo@gmail.com>
2026-04-18 22:28:42 -0400

    update FN_HHKB to do one-shot KC_LCTL on single tap

--- keyboards/ydkb/agar/keymaps/djcastaldo/keymap.c
+++ keyboards/ydkb/agar/keymaps/djcastaldo/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_60_tsangan_hhkb(
         DUAL_ESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, DYN_LT,
         TMUX_LT, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, BSPCFAST,
-        FN_HHKB_2, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        FN_HHKB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_TD, OSL_FKEY,
         _______, TD(LGUI_OSL), KC_LALT, KC_SPC,  RALT_TD, SP_RCTL, _______
     ),
